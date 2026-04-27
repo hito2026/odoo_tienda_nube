724cae45f99202f8e01a752b1c265b1191909b63
MatiasBanega <banegamatias@gmail.com>
2026-04-27 17:46:14 -0300

    [FIX] mejoras

--- odoo_tienda_nube/controllers/get_image_product_template_ids.py
+++ odoo_tienda_nube/controllers/get_image_product_template_ids.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import request
 import base64
-from odoo.http import request # -*- coding: utf-8 -*-
 import logging
 import io
-import imghdr
 from PIL import Image
+
 _logger = logging.getLogger(__name__)
 
 class PublicController(http.Controller):
@@ -12,8 +13,14 @@ class PublicController(http.Controller):
     @http.route('/ati_tn_product_template_ids/<int:id>', type='http', auth='public')
     def serve_image(self, id, **kwargs):
         record = request.env['product.product'].sudo().browse(id)
-        binary_data = base64.b64decode(record.image_1920) 
-        image_format = imghdr.what(None, binary_data) or 'png'
+        binary_data = base64.b64decode(record.image_1920)
+        
+        # Detectar formato de imagen usando PIL en lugar de imghdr (deprecated)
+        try:
+            img = Image.open(io.BytesIO(binary_data))
+            image_format = img.format or 'png'
+        except Exception:
+            image_format = 'png'
 
         headers = [('Content-Type', f'image/{image_format.lower()}')]
         return request.make_response(binary_data, headers)
@@ -21,8 +28,14 @@ class PublicController(http.Controller):
     @http.route('/ati_tn_product_template_galery_ids/<int:id>', type='http', auth='public')
     def serve_image_galery(self, id, **kwargs):
         record = request.env['product.image.tn'].sudo().browse(id)
-        binary_data = base64.b64decode(record.image_1920) 
-        image_format = imghdr.what(None, binary_data) or 'png'
+        binary_data = base64.b64decode(record.image_1920)
+        
+        # Detectar formato de imagen usando PIL en lugar de imghdr (deprecated)
+        try:
+            img = Image.open(io.BytesIO(binary_data))
+            image_format = img.format or 'png'
+        except Exception:
+            image_format = 'png'
     
         headers = [('Content-Type', f'image/{image_format.lower()}')]
         return request.make_response(binary_data, headers)
