a33d6735832297ecc2decfd1c7eaae524f32f11c
MatiasBanega <banegamatias@gmail.com>
2026-04-27 22:32:08 -0300

    [FIX] restaura botones en header y mueve campos TN a page

--- odoo_tienda_nube/__manifest__.py
+++ odoo_tienda_nube/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": """
         Conector de Tienda Nube con Odoo""",
     "category": "Sale",
-    "version": "18.0.7.0.8",
+    "version": "18.0.7.0.9",
     "website": "https://devoo.io",
     "author": "Iván Arriola | Valentin Romero - Devoo",
     "license": "LGPL-3",
