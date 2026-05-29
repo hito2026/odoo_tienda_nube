694444c19eeee92874d73933b815625989a26414
MatiasBanega <banegamatias@gmail.com>
2026-05-29 00:12:47 -0300

    [IMP] Configure TN invoicing trigger on pick or out

--- odoo_tienda_nube_auto_account/models/stock_picking_inherit.py
+++ odoo_tienda_nube_auto_account/models/stock_picking_inherit.py
@@ -4,13 +4,48 @@ from odoo import models
 class StockPickingTnAutoAccount(models.Model):
     _inherit = "stock.picking"
 
+    def _tn_should_trigger_auto_flow(self, order):
+        self.ensure_one()
+
+        mode = order.company_id.tn_auto_invoice_trigger or "out"
+        is_outgoing = self.picking_type_id.code == "outgoing"
+        is_internal = self.picking_type_id.code == "internal"
+
+        if mode == "out":
+            if not is_outgoing:
+                return False
+            pending_out_pickings = order.picking_ids.filtered(
+                lambda current: current.picking_type_id.code == "outgoing"
+                and current.state not in ("done", "cancel")
+            )
+            return not pending_out_pickings
+
+        internal_pickings = order.picking_ids.filtered(
+            lambda current: current.picking_type_id.code == "internal"
+            and current.state != "cancel"
+        )
+        if internal_pickings:
+            if not is_internal:
+                return False
+            pending_internal_pickings = internal_pickings.filtered(
+                lambda current: current.state not in ("done", "cancel")
+            )
+            return not pending_internal_pickings
+
+        if not is_outgoing:
+            return False
+
+        pending_out_pickings = order.picking_ids.filtered(
+            lambda current: current.picking_type_id.code == "outgoing"
+            and current.state not in ("done", "cancel")
+        )
+        return not pending_out_pickings
+
     def button_validate(self):
         result = super().button_validate()
 
         for picking in self:
             if picking.state != "done":
-                continue
-            if picking.picking_type_id.code != "outgoing":
                 continue
             if not picking.sale_id:
                 continue
@@ -19,11 +54,7 @@ class StockPickingTnAutoAccount(models.Model):
             if not order.id_tn:
                 continue
 
-            pending_out_pickings = order.picking_ids.filtered(
-                lambda current: current.picking_type_id.code == "outgoing"
-                and current.state not in ("done", "cancel")
-            )
-            if pending_out_pickings:
+            if not picking._tn_should_trigger_auto_flow(order):
                 continue
 
             order._tn_run_auto_billing_and_payment()
